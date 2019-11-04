98ce0d4e06e0edfaaf916d4d28ee306b1e44e83a
Bruno A. Caceres <brunoacaceres@gmail.com>
2019-11-03 22:18:18 -0400

    Instalando Mongoose e conectando ao MongoDB

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,9 @@
 import server from './server'
-
+import { connect } from './models'
+// console.log('MONGO_URI: ', process.env.MONGO_URI)
 const { PORT: port = 4000 } = process.env
-
 const main = async (): Promise<void> => {
+  await connect()
   await server.start({ port })
   console.log(`Listening at port ${port}...`)
 }
