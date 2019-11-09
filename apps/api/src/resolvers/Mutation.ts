8cdae23ce1457390c41c4bd4c169accc71a20eb3
Bruno A. Caceres <brunoacaceres@gmail.com>
2019-11-09 05:55:47 -0400

    Mutation para deletar produtos por id

--- apps/api/src/resolvers/Mutation.ts
+++ apps/api/src/resolvers/Mutation.ts
@@ -1,14 +1,19 @@
-import { ProductCreateInput, Resolver } from '../types'
+import { ProductCreateInput, ProductDeleteInput, Resolver } from '../types'
 
 const createProduct: Resolver<ProductCreateInput> = (_, args, { db }) => {
   const { Product } = db
   const { data } = args
   const product = new Product(data)
-  console.log('args: ', args)
-  console.log('product: ', product)
   return product.save()
+}
+
+const deleteProduct: Resolver<ProductDeleteInput> = (_, args, { db }) => {
+  const { Product } = db
+  const { _id } = args
+  return Product.findByIdAndDelete(_id).exec()
 }
 
 export default {
   createProduct,
+  deleteProduct,
 }
