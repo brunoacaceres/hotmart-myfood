651590dea4300eab68dbc99e2f8968166fc50459
Bruno A. Caceres <brunoacaceres@gmail.com>
2019-11-10 17:53:09 -0400

    Query para buscar Pedido por id

--- apps/api/src/resolvers/Query.ts
+++ apps/api/src/resolvers/Query.ts
@@ -1,4 +1,11 @@
-import { Resolver, ProductByIdInput, UserRole, ProductDocument } from '../types'
+import {
+  ProductByIdInput,
+  Resolver,
+  UserRole,
+  OrderByIdInput,
+  ProductDocument,
+  OrderDocument,
+} from '../types'
 import { findDocument } from '../utils'
 
 const orders: Resolver<{}> = (_, args, { db, authUser }) => {
@@ -6,6 +13,19 @@ const orders: Resolver<{}> = (_, args, { db, authUser }) => {
   const { Order } = db
   const conditions = role === UserRole.USER ? { user: _id } : {}
   return Order.find(conditions)
+}
+
+const order: Resolver<OrderByIdInput> = (_, args, { db, authUser }) => {
+  const { _id } = args
+  const { _id: userId, role } = authUser
+  const where = role === UserRole.USER ? { user: userId, _id } : null
+  return findDocument<OrderDocument>({
+    db,
+    model: 'Order',
+    field: '_id',
+    value: _id,
+    where,
+  })
 }
 
 const products: Resolver<{}> = (_, args, { db }) => db.Product.find()
@@ -21,6 +41,7 @@ const product: Resolver<ProductByIdInput> = async (_, args, { db }) => {
 
 export default {
   orders,
+  order,
   products,
   product,
 }
